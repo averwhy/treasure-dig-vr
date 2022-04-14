7f90427910fa03b936ec454bd266e7543d35201e
averyB <averwhy@gmail.com>
2022-04-14 12:56:27 -0400

    Sand duplicating is working!

--- scenes/Blocks.gd
+++ scenes/Blocks.gd
@@ -1,21 +1,22 @@
 extends Spatial
 
+var sand = preload("res://scenes/Sand.tscn")
 var loading_blocks: bool = true
 var added_blocks = []
+const default_sand_position = Vector3(-1.897, 111.306, -10.729)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var children: Array = self.get_children()
 	var iter = 0
 	var offset = 2.009
 	while iter < 10:
-		for block in children:
-			vr.show_dbg_info("blocks","running, iter %s" % iter)
-			var new_block = self.duplicate(4)
-			new_block.translate(Vector3(block.translation.x, (block.translation.y + offset), block.translation.z))
+		var new_block = sand.instance()
+		self.add_child(new_block)
+		new_block.translate(Vector3(0, offset, 0))
 		iter += 1
 		offset += 2.009
-	vr.show_dbg_info("blocks", "stopped")
+		vr.log_info("added a block at position %s" % new_block.translation)
+		added_blocks.append(new_block)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
