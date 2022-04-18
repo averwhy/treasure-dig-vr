8115db07d397a1e64918da9177dbc7a88e462f0c
averyB <averwhy@gmail.com>
2022-04-17 23:38:19 -0400

    begin work on scripting all sand blocks

--- scenes/Blocks.gd
+++ scenes/Blocks.gd
@@ -2,17 +2,36 @@ extends Spatial
 
 var sand = preload("res://scenes/Sand.tscn")
 var loading_blocks: bool = true
+var top_level_blocks = []
 var added_blocks = []
+
+func add_block(offset: Vector3, parent: Spatial = self):
+	"""Adds a new sand block.
+	
+	Parameters
+	----------
+	offset : Vector3
+	   	The offset used in translating the sand block. Note that it will not move it to this position,
+		it will only add this offset to the current translation.
+	parent : Spatial, optional
+		The Spatial node to set the sand block
+		
+	Returns
+	-------
+	StaticBody
+		The new instance of the sand block.
+	"""
+	var new_block = sand.instance()
+	parent.add_child(new_block)
+	new_block.translate(offset)
+	return new_block
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var iter = 0
 	var offset = 0
-	vr.show_dbg_info("loading blocks", loading_blocks)
 	while iter < 10:
-		var new_block = sand.instance()
-		self.add_child(new_block)
-		new_block.translate(Vector3(0, offset, 0))
+		var new_block = add_block(Vector3(0, offset, 0))
 		iter += 1
 		offset -= 2.0
 		vr.log_info("added a block at position %s" % new_block.translation)
