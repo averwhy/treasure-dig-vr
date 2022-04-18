fe839f4c49c3bf593a4d82b069e3d4ef3e3b0de0
averyB <averwhy@gmail.com>
2022-04-18 09:43:34 -0400

    Working on all blocks being made
    top level is working, lower levels arent

--- scenes/Blocks.gd
+++ scenes/Blocks.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var sand = preload("res://scenes/Sand.tscn")
+const sand = preload("res://scenes/Sand.tscn")
 var loading_blocks: bool = true
 var top_level_blocks = []
 var added_blocks = []
@@ -14,7 +14,7 @@ func add_block(offset: Vector3, parent: Spatial = self):
 	   	The offset used in translating the sand block. Note that it will not move it to this position,
 		it will only add this offset to the current translation.
 	parent : Spatial, optional
-		The Spatial node to set the sand block
+		The Spatial node to set the sand block. Defaults to `self`.
 		
 	Returns
 	-------
@@ -28,13 +28,23 @@ func add_block(offset: Vector3, parent: Spatial = self):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	top_level_blocks = [
+		add_block(Vector3(0, 0, 0)),
+		add_block(Vector3(0, 0, -2)),
+		add_block(Vector3(0, 0, -4)),
+		add_block(Vector3(2, 0, 0)),
+		add_block(Vector3(4, 0, 0)),
+		add_block(Vector3(2, 0, -2)),
+		add_block(Vector3(4, 0, -4)),
+		add_block(Vector3(4, 0, -2)),
+		add_block(Vector3(2, 0, -4))
+	]
+	
 	var iter = 0
-	var offset = 0
-	while iter < 10:
-		var new_block = add_block(Vector3(0, offset, 0))
-		iter += 1
-		offset -= 2.0
-		vr.log_info("added a block at position %s" % new_block.translation)
+	var offset = -2.0
+	for block in top_level_blocks:
+		vr.show_dbg_info("blockpos", "%s" % block.translation.y + offset)
+		var new_block = add_block((block.translation.y + offset))
 		added_blocks.append(new_block)
 	loading_blocks = false
 
